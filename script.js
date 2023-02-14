379134f05e564210fe01cd6dc41ef0e2998bbcc9
groot96 <104350826+groot96@users.noreply.github.com>
2023-02-14 21:51:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
+var todoValue = document.getElementById("newTodoInput");
+var btn = document.getElementById("addTodoBtn");
+var ol  = document.getElementById("todoList")
 
+function addTodo() {
+	if(todoValue.value){
+	// ned to create list (li)
+
+	var li = document.createElement('li');
+	li.textContent = todoValue.value;
+	ol.appendChild(li);
+		todoValue.value="";
+ }
+	
+}
+
+btn.addEventListener("click",addTodo);
